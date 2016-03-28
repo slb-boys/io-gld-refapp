e96f60a656f727786c60dfdbbd99a1f603ffcdf3
Dan Dees <dand@appsmiths.com>
2016-03-28 13:40:52 +0800

    initial files

---
+++ io-gld-refapp/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace io_gld_refapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.startGlue = new System.Windows.Forms.Button();
+            this.pullModel = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.07692F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(313, 262);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.textBox1);
+            this.flowLayoutPanel1.Controls.Add(this.outputBox);
+            this.flowLayoutPanel1.Controls.Add(this.startGlue);
+            this.flowLayoutPanel1.Controls.Add(this.pullModel);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(307, 256);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(307, 0);
+            this.textBox1.TabIndex = 0;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.outputBox.Location = new System.Drawing.Point(3, 9);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputBox.Size = new System.Drawing.Size(304, 86);
+            this.outputBox.TabIndex = 1;
+            this.outputBox.TextChanged += new System.EventHandler(this.outputBox_TextChanged);
+            // 
+            // startGlue
+            // 
+            this.startGlue.AutoSize = true;
+            this.startGlue.Location = new System.Drawing.Point(3, 101);
+            this.startGlue.Name = "startGlue";
+            this.startGlue.Size = new System.Drawing.Size(80, 27);
+            this.startGlue.TabIndex = 2;
+            this.startGlue.Text = "WinGLUE";
+            this.startGlue.UseVisualStyleBackColor = true;
+            this.startGlue.Click += new System.EventHandler(this.startGlue_Click);
+            // 
+            // pullModel
+            // 
+            this.pullModel.AutoSize = true;
+            this.pullModel.Location = new System.Drawing.Point(89, 101);
+            this.pullModel.Name = "pullModel";
+            this.pullModel.Size = new System.Drawing.Size(83, 27);
+            this.pullModel.TabIndex = 3;
+            this.pullModel.Text = "Pull Model";
+            this.pullModel.UseVisualStyleBackColor = true;
+            this.pullModel.Click += new System.EventHandler(this.pullModel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 262);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "IO GLD Ref App";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button startGlue;
+        private System.Windows.Forms.Button pullModel;
+    }
+}
+
---
+++ io-gld-refapp/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// 
+using JsonRPC;
+using Newtonsoft.Json.Linq;
+
+namespace io_gld_refapp
+{
+    public partial class Form1 : Form
+    {
+        private int ii = 0 ;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startGlue_Click(object sender, EventArgs e)
+        {
+            if (this.ii==0)
+            {
+                outputBox.Text = ii.ToString();
+            }
+            else
+            {
+                outputBox.AppendText("\r\n" + ii.ToString());
+            }
+            ii++;
+        
+        }
+
+        private void outputBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pullModel_Click(object sender, EventArgs e)
+        {
+            using (Client rpcClient = new Client("http://localhost:8080"))
+            {
+                Request rq = rpcClient.NewRequest("getGraphList");
+                GenericResponse response = rpcClient.Rpc(rq);
+
+                if (response.Result == null)
+                {
+                    Console.WriteLine(string.Format("Error in response, code:{0} message:{1}",
+                        response.Error.Code, response.Error.Message));
+                }
+                JToken result = response.Result;
+                outputBox.AppendText(result.FirstOrDefault().ToString());
+
+            }
+        }
+    }
+}
+
+public static class WinFormsExtensions
+{
+    public static void AppendLine(this TextBox source, string value)
+    {
+        if (source.Text.Length == 0)
+            source.Text = value;
+        else
+            source.AppendText("\r\n" + value);
+    }
+}
